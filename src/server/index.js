0207bfa4ef7232339ab5b303f9cf8e0598e09f85
abuaesh <noha.abuaesh@gmail.com>
2020-07-02 22:18:57 +0300

    Working on the oracles server

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,35 @@
+import OraclesServer from './server';
 
-import http from 'http'
+ 
+ var http = require('http');
+ var server = http.createServer(function(request, response){
+
+    // Instantiate oracles server:
+    let oraclesServer = new OraclesServer('localhost', () => {
+        // Server boot-up message:
+            response.writeHead(200, {'Content-Type':'text/html'});
+            let oraclesCount = 0; //OraclesCount
+            oraclesServer.oraclesCount((OC)=>{
+                oraclesCount = OC;
+                console.log(oraclesCount);
+                });
+            var message = '<h1>Oracles Server</h1>'+
+                '<h3>Setting up '+ oraclesCount + ' oracles for fetching flight status:</h3>';
+            response.end(message);
+
+            message = ''; // reset server message
+            console.log(message)  ;
+            
+            response.writeHead(200, {'Content-Type':'text/html'});
+            response.end(message);
+        
+        }); //end oraclesServer
+    }); //end server
+ server.listen(3000,'127.0.0.1');
+ console.log('Oracles Server up and running');
+
+
+/*import http from 'http'
 import app from './server'
 
 const server = http.createServer(app)
@@ -13,3 +43,4 @@ if (module.hot) {
   currentApp = app
  })
 }
+*/
--- src/server/server.js
+++ src/server/server.js
@@ -1,52 +1,95 @@
-import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
+/*import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
 import Config from './config.json';
 import Web3 from 'web3';
 import express from 'express';
+*/
+import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
+import Config from './config.json';
+import Web3 from 'web3';
 
-async () => {
-  // Define workspace variables
-  let config = Config['localhost'];
-  let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
-  web3.eth.defaultAccount = web3.eth.accounts[0];
-  let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
-
-  // Register 20 oracles
-  const TEST_ORACLES_COUNT = 20;
-  const FIRST_ORACLE_ADDRESS = web3.eth.accounts.length - TEST_ORACLES_COUNT;
-
-  let fee = await flightSuretyApp.REGISTRATION_FEE.call();
-
-  for(let a=FIRST_ORACLE_ADDRESS; a<TEST_ORACLES_COUNT+FIRST_ORACLE_ADDRESS; a++) {      
-    await flightSuretyApp.registerOracle({from: accounts[a], value: fee});
-    let result = await flightSuretyApp.getMyIndexes.call({from: accounts[a]});
-    console.log(`Oracle Registered: ${result[0]}, ${result[1]}, ${result[2]}`);
+export default class OraclesServer {
+  constructor(network, callback) {
+    // Define workspace variables
+    let config = Config[network];
+    //this.web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
+    this.web3 = new Web3(new Web3.providers.HttpProvider(config.url));
+    this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+    //this.web3.eth.defaultAccount = this.web3.eth.accounts[0];
+    //this.flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+    this.oracles = [];
+    this.initialize(callback);
+    this.ORACLES_COUNT = 0;
   }
 
-  // Listen to requests for oracles
-  flightSuretyApp.events.OracleRequest({
-      fromBlock: 0
-    }, function (error, event) {
-      if (error) console.log(error)
-      console.log(event)
-  }).on('data', function(event){
-    console.log(event); // same results as the optional callback above
-    console.log('Listened to the Orace Request..')
-  })
-  .on('changed', function(event){
-    // remove event from local database
+  initialize(callback){
+    this.web3.eth.getAccounts((error, accounts) => {
+      // Register 20 oracles
+      this.ORACLES_COUNT = 15;
+      this.FIRST_ORACLE_ADDRESS = this.web3.eth.accounts.length - this.ORACLES_COUNT;
+      this.LAST_ORACLE_ADDRESS = this.ORACLES_COUNT + this.FIRST_ORACLE_ADDRESS;
+      // Initialize oracles addresses and indexes with smart contract
+      this.flightSuretyApp.REGISTRATION_FEE.call((err, fee)=>{
+        console.log(fee);
+        for(let a=this.FIRST_ORACLE_ADDRESS; a<this.LAST_ORACLE_ADDRESS; a++) {      
+          this.flightSuretyApp.registerOracle({from: accounts[a], value: fee},()=>{
+            this.oracles.push(accounts[a]);
+            console.log(accounts[a]);
+          });
 
-  })
-  .on('error', console.error);
+        }
+      });
+    });
+  }//end initialize
 
+  oraclesCount(callback){
+    console.log('oracles count is called.. Returning '+ this.ORACLES_COUNT);
+    callback(this.ORACLES_COUNT);
+  }
+
+}//end Server
+ 
+ 
+ /*
   
-};
 
-const app = express();
-  app.get('/api', (req, res) => {
-      res.send({
-        message: 'An API for use with your Dapp!'
-      })
-  })
+// Display oracles addresses and indexes previously retrieved from smart contract
+for(let a=FIRST_ORACLE_ADDRESS; a<ORACLES_COUNT+FIRST_ORACLE_ADDRESS; a++) {  
+    let result = await flightSuretyApp.getMyIndexes.call({from: accounts[a]});
+    message = `Oracle Registered: ${result[0]}, ${result[1]}, ${result[2]}`;
+    console.log(message);
+  }
+}
+  /////////////////////////////////////////////////////////////////////////////////////////
+  /*
+    
 
-export default app;
+    // Listen to requests for oracles
+    flightSuretyApp.events.OracleRequest({
+        fromBlock: 0
+      }, function (error, event) {
+        if (error) console.log(error)
+        console.log(event)
+    }).on('data', function(event){
+      console.log(event); // same results as the optional callback above
+      console.log('Listened to the Orace Request..')
+    })
+    .on('changed', function(event){
+      // remove event from local database
 
+    })
+    .on('error', console.error);
+
+    
+  };
+
+  const app = express();
+    app.get('/api', (req, res) => {
+      console.log('Server running...');
+        res.send({
+          message: 'An API for use with your Dapp!'
+        })
+    })
+
+  export default app;
+  
+*/
