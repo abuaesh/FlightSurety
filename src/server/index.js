ab4c4d305d8cf71415dccf9eb9b501f8dd351834
abuaesh <noha.abuaesh@gmail.com>
2020-07-06 23:19:50 +0300

    Working on the oracles server

--- src/server/index.js
+++ src/server/index.js
@@ -1,35 +1,5 @@
-import OraclesServer from './server';
-
- 
- var http = require('http');
- var server = http.createServer(function(request, response){
-
-    // Instantiate oracles server:
-    let oraclesServer = new OraclesServer('localhost', () => {
-        // Server boot-up message:
-            response.writeHead(200, {'Content-Type':'text/html'});
-            let oraclesCount = 0; //OraclesCount
-            oraclesServer.oraclesCount((OC)=>{
-                oraclesCount = OC;
-                console.log(oraclesCount);
-                });
-            var message = '<h1>Oracles Server</h1>'+
-                '<h3>Setting up '+ oraclesCount + ' oracles for fetching flight status:</h3>';
-            response.end(message);
-
-            message = ''; // reset server message
-            console.log(message)  ;
-            
-            response.writeHead(200, {'Content-Type':'text/html'});
-            response.end(message);
-        
-        }); //end oraclesServer
-    }); //end server
- server.listen(3000,'127.0.0.1');
- console.log('Oracles Server up and running');
-
-
-/*import http from 'http'
+/*
+import http from 'http'
 import app from './server'
 
 const server = http.createServer(app)
@@ -44,3 +14,30 @@ if (module.hot) {
  })
 }
 */
+
+/*import OraclesServer from './server';
+
+ 
+ var http = require('http');
+ var server = http.createServer(function(request, response){
+
+    
+    }); //end server
+ server.listen(3000,'127.0.0.1');
+ console.log('Oracles Server up and running');
+*/
+
+import http from 'http'
+import app from './server'
+
+const server = http.createServer(app)
+let currentApp = app
+server.listen(3000)
+
+if (module.hot) {
+ module.hot.accept('./server', () => {
+  server.removeListener('request', currentApp)
+  server.on('request', app)
+  currentApp = app
+ })
+}
--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,62 @@
+/*
+import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
+import Config from './config.json';
+import Web3 from 'web3';
+import express from 'express';
+
+
+let config = Config['localhost'];
+let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
+web3.eth.defaultAccount = web3.eth.accounts[0];
+let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+
+const ORACLES_COUNT = 15;
+const FIRST_ORACLE_ADDRESS = web3.eth.accounts.length - ORACLES_COUNT;
+const LAST_ORACLE_ADDRESS = ORACLES_COUNT + FIRST_ORACLE_ADDRESS;
+console.log('There will be '+ ORACLES_COUNT+' oracles.');
+// Initialize oracles addresses and indexes with smart contract
+console.log('The last oracle has the address: '+ LAST_ORACLE_ADDRESS);
+
+flightSuretyApp.methods.getRegistrationFee().call((error,result) => { 
+  console.log(reult);
+  for(let a=FIRST_ORACLE_ADDRESS; a<LAST_ORACLE_ADDRESS; a++) {      
+    flightSuretyApp.methods.registerOracle().send({from: accounts[a], value: result},()=>{
+      //oracles.push(accounts[a]);
+      console.log(accounts[a]);
+    });
+  }//end for
+});//end REGISTRATION_FEE
+
+
+flightSuretyApp.events.OracleRequest({
+    fromBlock: 0
+  }, function (error, event) {
+    if (error) console.log(error)
+    console.log(event)
+});
+
+const app = express();
+app.get('/api', (req, res) => {
+    res.send({
+      message: 'An API for use with your Dapp!'
+    })
+    console.log('halo..');
+})
+
+export default app;
+
 /*import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
 import Config from './config.json';
 import Web3 from 'web3';
 import express from 'express';
 */
+
 import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
 import Config from './config.json';
 import Web3 from 'web3';
+import express from 'express';
 
-export default class OraclesServer {
+class OraclesServer {
   constructor(network, callback) {
     // Define workspace variables
     let config = Config[network];
@@ -22,23 +71,25 @@ export default class OraclesServer {
   }
 
   initialize(callback){
-    this.web3.eth.getAccounts((error, accounts) => {
+    this.web3.eth.getAccounts((accounts) => {
       // Register 20 oracles
       this.ORACLES_COUNT = 15;
       this.FIRST_ORACLE_ADDRESS = this.web3.eth.accounts.length - this.ORACLES_COUNT;
       this.LAST_ORACLE_ADDRESS = this.ORACLES_COUNT + this.FIRST_ORACLE_ADDRESS;
+      console.log('There will be '+this.ORACLES_COUNT+' oracles.');
       // Initialize oracles addresses and indexes with smart contract
-      this.flightSuretyApp.REGISTRATION_FEE.call((err, fee)=>{
+      console.log('The last oracle has the address: '+this.LAST_ORACLE_ADDRESS);
+
+      /*this.flightSuretyApp.methods.REGISTRATION_FEE.call((fee) => { 
         console.log(fee);
         for(let a=this.FIRST_ORACLE_ADDRESS; a<this.LAST_ORACLE_ADDRESS; a++) {      
-          this.flightSuretyApp.registerOracle({from: accounts[a], value: fee},()=>{
+          this.flightSuretyApp.methods.registerOracle.send({from: accounts[a], value: fee},()=>{
             this.oracles.push(accounts[a]);
             console.log(accounts[a]);
           });
-
-        }
-      });
-    });
+        }//end for
+      });//end REGISTRATION_FEE */
+    });//end getAccounts
   }//end initialize
 
   oraclesCount(callback){
@@ -46,12 +97,10 @@ export default class OraclesServer {
     callback(this.ORACLES_COUNT);
   }
 
-}//end Server
+}//end OraclesServer Class
  
  
  /*
-  
-
 // Display oracles addresses and indexes previously retrieved from smart contract
 for(let a=FIRST_ORACLE_ADDRESS; a<ORACLES_COUNT+FIRST_ORACLE_ADDRESS; a++) {  
     let result = await flightSuretyApp.getMyIndexes.call({from: accounts[a]});
@@ -81,10 +130,30 @@ for(let a=FIRST_ORACLE_ADDRESS; a<ORACLES_COUNT+FIRST_ORACLE_ADDRESS; a++) {
 
     
   };
-
+*/
   const app = express();
     app.get('/api', (req, res) => {
-      console.log('Server running...');
+     /* console.log('Server running...');
+      // Instantiate oracles server:
+      let oraclesServer = new OraclesServer('localhost', () => {
+        // Server boot-up message:
+        response.writeHead(200, {'Content-Type':'text/html'});
+        let oraclesCount = 0; //OraclesCount
+        oraclesServer.oraclesCount((OC)=>{
+            oraclesCount = OC;
+            console.log(oraclesCount);
+            });
+        var message = '<h1>Oracles Server</h1>'+
+            '<h3>Setting up '+ oraclesCount + ' oracles for fetching flight status:</h3>';
+        response.end(message);
+
+        message = ''; // reset server message
+        console.log(message)  ;
+        
+        response.writeHead(200, {'Content-Type':'text/html'});
+        response.end(message);
+      }); //end oraclesServer
+*/
         res.send({
           message: 'An API for use with your Dapp!'
         })
@@ -92,4 +161,3 @@ for(let a=FIRST_ORACLE_ADDRESS; a<ORACLES_COUNT+FIRST_ORACLE_ADDRESS; a++) {
 
   export default app;
   
-*/
