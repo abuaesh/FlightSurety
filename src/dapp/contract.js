863f1fbe26546f7549612f36f4073e78b032685a
abuaesh <noha.abuaesh@gmail.com>
2020-07-07 14:25:43 +0300

    Working on event listening in the oracles server

--- src/dapp/contract.js
+++ src/dapp/contract.js
@@ -70,9 +70,9 @@ export default class Contract {
         .on('error', console.error);*/
 
         self.flightSuretyApp.once('OracleRequest', function(error, event){
-            console.log('Listened to the new oracle event.........'+event);
-            if (error) console.log(error);
-        })
+            console.log('Listened to the new oracle event. Returned: '+event+' Event.');
+            callback(error,payload);
+        });
     }
 
     buyInsurance(flight, amount, callback) {
--- src/server/server.js
+++ src/server/server.js
@@ -57,57 +57,85 @@ import Web3 from 'web3';
 import express from 'express';
 
 class OraclesServer {
-  constructor(network, callback) {
+  constructor(network) {
     // Define workspace variables
     let config = Config[network];
-    //this.web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
-    this.web3 = new Web3(new Web3.providers.HttpProvider(config.url));
+    this.web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
+    //this.web3 = new Web3(new Web3.providers.HttpProvider(config.url));
     this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
     //this.web3.eth.defaultAccount = this.web3.eth.accounts[0];
     //this.flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
     this.oracles = [];
-    this.initialize(callback);
-    this.ORACLES_COUNT = 0;
+    this.initialize();
+    //this.ORACLES_COUNT = 0;
   }
 
-  initialize(callback){
-    this.web3.eth.getAccounts((accounts) => {
+  initialize(){
+    this.web3.eth.getAccounts().then(accounts => {
       // Register 20 oracles
-      this.ORACLES_COUNT = 15;
-      this.FIRST_ORACLE_ADDRESS = this.web3.eth.accounts.length - this.ORACLES_COUNT;
+      this.ORACLES_COUNT = 20;
+      this.FIRST_ORACLE_ADDRESS = accounts.length - this.ORACLES_COUNT -1;
       this.LAST_ORACLE_ADDRESS = this.ORACLES_COUNT + this.FIRST_ORACLE_ADDRESS;
-      console.log('There will be '+this.ORACLES_COUNT+' oracles.');
-      // Initialize oracles addresses and indexes with smart contract
-      console.log('The last oracle has the address: '+this.LAST_ORACLE_ADDRESS);
+      console.log('Ganache returned '+accounts.length+' accounts.');
+      console.log('Server will use only '+this.ORACLES_COUNT+' of these accounts for oracles.');
+      console.log('Starting from accounts['+this.FIRST_ORACLE_ADDRESS+'] for the first oracle.');
+      console.log('Ending at accounts['+this.LAST_ORACLE_ADDRESS+'] for the last oracle.');
 
-      /*this.flightSuretyApp.methods.REGISTRATION_FEE.call((fee) => { 
-        console.log(fee);
-        for(let a=this.FIRST_ORACLE_ADDRESS; a<this.LAST_ORACLE_ADDRESS; a++) {      
-          this.flightSuretyApp.methods.registerOracle.send({from: accounts[a], value: fee},()=>{
-            this.oracles.push(accounts[a]);
-            console.log(accounts[a]);
-          });
-        }//end for
-      });//end REGISTRATION_FEE */
+      // Initialize oracles addresses and indexes with smart contract
+      this.flightSuretyApp.methods.REGISTRATION_FEE().call((fee) => { 
+        fee = Math.pow(10, 18); //See why registration fee is not being retrieved from smart contract
+        console.log('Smart Contract requires (' +fee+') ethers to fund oracle registration.');
+        accounts.forEach(account => {
+          this.oracles.push(account); //To keep the server updated with oracles addresses 
+                                      //Because sometimes the oracle is already registered in the contract from before, 
+                                      //so it reverts when the server tries to register it again.
+          this.flightSuretyApp.methods.registerOracle().send({
+                "from": account,
+                "value": 1000000000000000000,
+                "gas": 4712388,
+                "gasPrice": 100000000000
+          }).then(result => {
+              //oracle created;
+              console.log('Registered: '+account);
+          }).catch(err => {
+              // oracle errored
+              console.log('Could not create oracle at address: '+account+'\n\tbecause: '+err);
+          })
+       }); //end forEach account
+      
+        this.displayOracles();
+      });//end REGISTRATION_FEE 
     });//end getAccounts
+    
   }//end initialize
 
   oraclesCount(callback){
-    console.log('oracles count is called.. Returning '+ this.ORACLES_COUNT);
+    console.log('oraclesCount is called from server. Responding..\n There are '+ this.ORACLES_COUNT+' registered oracles.');
     callback(this.ORACLES_COUNT);
   }
 
-}//end OraclesServer Class
- 
- 
- /*
-// Display oracles addresses and indexes previously retrieved from smart contract
-for(let a=FIRST_ORACLE_ADDRESS; a<ORACLES_COUNT+FIRST_ORACLE_ADDRESS; a++) {  
-    let result = await flightSuretyApp.getMyIndexes.call({from: accounts[a]});
-    message = `Oracle Registered: ${result[0]}, ${result[1]}, ${result[2]}`;
-    console.log(message);
+  displayOracles(){
+    // Display oracles addresses and indexes previously retrieved from smart contract
+    this.oracles.forEach(oracle => {
+      this.flightSuretyApp.methods
+          .getMyIndexes().call({
+            "from": oracle
+          }).then(result => {
+            console.log('Assigned Indices: '+result[0]+', '+result[1]+', '+result[2]+'\t--for oracle: '+oracle);
+          }).catch(error=>{
+            console.log('Could not retrieve oracle indices because: '+error);
+          });
+    }); //end forEach oracle
+    
+    //Listen for oracleRequest event
+    this.flightSuretyApp.once('OracleRequest', event => {
+      console.log('Listened to the new oracle request event. Returned: '+event+' Event.');
+      //callback(error,payload);
+    });
   }
-}
+
+}//end OraclesServer Class
+
   /////////////////////////////////////////////////////////////////////////////////////////
   /*
     
@@ -131,29 +159,12 @@ for(let a=FIRST_ORACLE_ADDRESS; a<ORACLES_COUNT+FIRST_ORACLE_ADDRESS; a++) {
     
   };
 */
+    // Instantiate oracles server:
+      let oraclesServer = new OraclesServer('localhost'); 
+      oraclesServer.displayOracles();
+
   const app = express();
     app.get('/api', (req, res) => {
-     /* console.log('Server running...');
-      // Instantiate oracles server:
-      let oraclesServer = new OraclesServer('localhost', () => {
-        // Server boot-up message:
-        response.writeHead(200, {'Content-Type':'text/html'});
-        let oraclesCount = 0; //OraclesCount
-        oraclesServer.oraclesCount((OC)=>{
-            oraclesCount = OC;
-            console.log(oraclesCount);
-            });
-        var message = '<h1>Oracles Server</h1>'+
-            '<h3>Setting up '+ oraclesCount + ' oracles for fetching flight status:</h3>';
-        response.end(message);
-
-        message = ''; // reset server message
-        console.log(message)  ;
-        
-        response.writeHead(200, {'Content-Type':'text/html'});
-        response.end(message);
-      }); //end oraclesServer
-*/
         res.send({
           message: 'An API for use with your Dapp!'
         })
