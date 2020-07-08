a2b15b4f5e19271a947a48bde1194512e8cef82f
Daniel Abrao <danielpaladar@gmail.com>
2020-07-07 23:00:51 -0300

    Refactor backend code to use modern syntax

--- src/server/server.js
+++ src/server/server.js
@@ -1,121 +1,75 @@
+"use strict";
 
-import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
-import Config from './config.json';
-import Web3 from 'web3';
-import express from 'express';
+import FlightSuretyApp from "../../build/contracts/FlightSuretyApp.json";
+import Config from "./config.json";
+import Web3 from "web3";
+import express from "express";
 
 // Define workspace variables
-let network = 'localhost';
+let network = "localhost";
 let config = Config[network];
-let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
+let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace("http", "ws")));
 let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
 
-let oracles = [];
-var ORACLES_COUNT = 20, FIRST_ORACLE_ADDRESS, LAST_ORACLE_ADDRESS;
+const ORACLES_COUNT = 20;
+let FIRST_ORACLE_ADDRESS;
+let LAST_ORACLE_ADDRESS;
 
-web3.eth.getAccounts().then(accounts => {
-  // Register 20 oracles
-  FIRST_ORACLE_ADDRESS = accounts.length - ORACLES_COUNT -1;
-  LAST_ORACLE_ADDRESS = ORACLES_COUNT + FIRST_ORACLE_ADDRESS;
-  console.log('Ganache returned '+accounts.length+' accounts.');
-  console.log('Server will use only '+ORACLES_COUNT+' of these accounts for oracles.');
-  console.log('Starting from accounts['+FIRST_ORACLE_ADDRESS+'] for the first oracle.');
-  console.log('Ending at accounts['+LAST_ORACLE_ADDRESS+'] for the last oracle.');
+(async function () {
 
-  // Initialize oracles addresses and indexes with smart contract
-  flightSuretyApp.methods.REGISTRATION_FEE().call({
-    "from": accounts[0],
-    "gas": 4712388,
-    "gasPrice": 100000000000
-  }).then(fee => { 
-    console.log('Smart Contract requires ('+fee+') wei to fund oracle registration.');
-    accounts.forEach(account => {
-      oracles.push(account); //To keep the server updated with oracles addresses 
-                                  //Because sometimes the oracle is already registered in the contract from before, 
-                                  //so it reverts when the server tries to register it again.
-      flightSuretyApp.methods.registerOracle().send({
-            "from": account,
-            "value": fee,
-            "gas": 4712388,
-            "gasPrice": 100000000000
-      }).then(result => {
-          //oracle created;
-          //console.log('Registered: '+account);
-      }).catch(err => {
-          // oracle errored
-          //console.log('Could not create oracle at address: '+account+'\n\tbecause: '+err);
-      })
-    }); //end forEach account
+    let accounts = await web3.eth.getAccounts();
 
-    // Display oracles addresses and indexes previously retrieved from smart contract
-   /* oracles.forEach(oracle => {
-      flightSuretyApp.methods
-          .getMyIndexes().call({
-            "from": oracle,
-            "gas": 4712388,
-            "gasPrice": 100000000000
-          }).then(result => {
-            console.log('Assigned Indices: '+result[0]+', '+result[1]+', '+result[2]+'\tfor oracle: '+oracle);
+    FIRST_ORACLE_ADDRESS = accounts.length - ORACLES_COUNT -1;
+    LAST_ORACLE_ADDRESS = ORACLES_COUNT + FIRST_ORACLE_ADDRESS;
 
-          }).catch(error => {
-            console.log('Could not retrieve oracle indices because: '+error);
-          })
+    console.log(`Ganache returned ${accounts.length} accounts.`);
+    console.log(`Server will use only ${ORACLES_COUNT} of these accounts for oracles.`);
+    console.log(`Starting from accounts[${FIRST_ORACLE_ADDRESS}] for the first oracle.`);
+    console.log(`Ending at accounts[${LAST_ORACLE_ADDRESS}] for the last oracle.`);
 
-    }); //end forEach oracle*/
-
-    console.log('Oracles server all set-up...\nOracles registered and assigned addresses...');
-    console.log('Listening to a request event...');
-
-  //Listen for oracleRequest event
-
-  /* //1. 
-  flightSuretyApp.once('OracleRequest', event=>{
-        console.log('Listened to the new oracle event. Returned this event: ');
-        console.log(event);
-      });
-  */
-  /* //2. Senses the past event but not current ones that fire, Plus, event is undefined
-  flightSuretyApp.getPastEvents('OracleRequest', {fromBlock: 'latest'},
-    function(error,events){}).then(function(error, events){ 
-      if(error) console.log(error);
-      console.log('Caught an event: ');
-      console.log(events); });
-  */
-
-  /* //3.
-  flightSuretyApp.events.allEvents()
-    .on('data', (event) => {
-      console.log(event);
-    })
-    .on('error', console.error);
-  */
-  
-  /* //4. 
-  flightSuretyApp.events.allEvents({fromBlock: 'latest'}, 
-        function(error, event){
-          if(error) console.log(error);
-          console.log('Caught an event: ');
-          console.log(events);
-        });
-  */
-  
-  /* //5.
-  flightSuretyApp.events.OracleRequest({fromBlock: 'latest'}, 
-    function(error, event) {
-      if(error) console.log(error);
-      console.log('Caught an event: ');
-      console.log(event);
+    let fee = await flightSuretyApp.methods.REGISTRATION_FEE().call({
+        "from": accounts[0],
+        "gas": 4712388,
+        "gasPrice": 100000000000
     });
-  */
 
-  }).catch(err=>{console.log('Could not retrieve registration fee. '+err)});//end REGISTRATION_FEE 
-});//end getAccounts
+    console.log(`Smart Contract requires ${fee} wei to fund oracle registration.`);
+
+    await Promise.all(
+        accounts.map(account => {
+            return flightSuretyApp.methods.registerOracle().send({
+                "from": account,
+                "value": fee,
+                "gas": 4712388,
+                "gasPrice": 100000000000
+            });
+        })
+    );
+
+    console.log("Oracles server all set-up...\nOracles registered and assigned addresses...");
+    console.log("Listening to a request event...");
+
+    flightSuretyApp.events.OracleRequest({
+        "fromBlock": "latest"
+    }, (error, event) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(event);
+        }
+    });
+
+}());
+
+
+
+
 
   
 const app = express();
-app.get('/api', (req, res) => {
+app.get("/api", (req, res) => {
     res.send({
-      message: 'An API for use with your Dapp!'
+      message: "An API for use with your Dapp!"
     })
 })
 
